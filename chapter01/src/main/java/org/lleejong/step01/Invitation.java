2cde30078b1822610e17ec7814ff46653264de8c
lleejong <lleejong@gmail.com>
2019-07-31 08:40:57 +0900

    Add: Chapter1 step01

---
+++ chapter01/src/main/java/org/lleejong/step01/Invitation.java
@@ -0,0 +1,9 @@
+package org.lleejong.step01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+    private LocalDateTime when;
+
+}
---
+++ chapter01/src/main/java/org/lleejong/step01/Ticket.java
@@ -0,0 +1,10 @@
+package org.lleejong.step01;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
